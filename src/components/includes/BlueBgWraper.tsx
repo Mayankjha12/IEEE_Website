d5a3013f803e161167d9abe057a6fd4644f9572b
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2026-08-10 14:11:23 +0530

    Update BlueBgWraper.tsx

--- src/components/includes/BlueBgWraper.tsx
+++ src/components/includes/BlueBgWraper.tsx
@@ -7,7 +7,7 @@ export function BlueBgWrapper({
 }) {
   return (
     <section
-      className={`w-full py-16 md:py-24 bg-gradient-to-br from-[#002147] via-[#003865] to-[#006ba6] text-white ${className}`}
+      className={`w-full bg-gradient-to-br from-[#002147] via-[#003865] to-[#006ba6] text-white ${className}`}
     >
       <div className="mx-auto">{children}</div>
     </section>
